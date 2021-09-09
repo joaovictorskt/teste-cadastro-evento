e945bf1c5cbde46c0a26ae990ba8123ae7d403e3
joaovictorskt <joaobernardoskt@outlook.com>
2021-09-09 16:54:14 -0300

    subindo teste para github

---
+++ index.js
@@ -0,0 +1,18 @@
+var datadoevento = "23";
+
+    if (datadoevento <="23") {
+    console.log ("CADASTRO PERMITIDO "); console.log ("INSERIR SUA IDADE:");
+ }  else console.log ("CADASTRO NEGADO / DATA INVALIDA")
+   
+var idade = "18";
+
+    if (idade >="18") {
+    console.log ("IDADE PERMITIDA"); console.log ("INSIRA SEU NOME:")
+    } else  console.log ("IDADE NÃO PERMITIDA / CADASTRO NEGADO"); {
+
+    } 
+
+var listadeparticipantes = "100";
+   if (listadeparticipantes <"100"){
+       console.log ("Cadastro Concluído")
+   } else console.log ("CADASTRO NEGADO / LIMITE DE PARTICIPANTES ATINGIDO.");
