3f8d18081467bea384ad759cc62ae666f0c931ac
João Victor <89541042+joaovictorskt@users.noreply.github.com>
2021-09-10 11:23:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ var idade = "18";
     } 
 
 var listadeparticipantes = "100";
-   if (listadeparticipantes <"100"){
-       console.log ("Cadastro Concluído")
-   } else console.log ("CADASTRO NEGADO / LIMITE DE PARTICIPANTES ATINGIDO.");
+   if (listadeparticipantes >"100"){
+       console.log ("CADASTRO NEGADO"); console.log ("LIMITE DE PARTICIPANTES ATINGIDO")
+   } else console.log ("CADASTRO CONCLUÍDO);
